87f229a9af60ac62cc0b990a76b963e9bb49d86d
pji0219 <pji219@naver.com>
2022-10-19 15:20:40 +0900

    회원가입 페이지 추가!

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Index from './pages/Index';
 import Item from './pages/Item';
 import Join from './pages/Join';
 import Login from './pages/Login';
+import Signup2 from './pages/Signup2';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/item" element={<Item />} />
-      <Route path="/join" element={<Join />} />
-      <Route path="/login" element={<Login />} />
+      <Route path='/' element={<Index />} />
+      <Route path='/item' element={<Item />} />
+      <Route path='/join' element={<Join />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup2' element={<Signup2 />} />
     </Routes>
   );
 }
