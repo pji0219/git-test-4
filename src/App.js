709d63fde1bae4adf43eaadc09782963b03cf6ff
Tetz <xenosign@naver.com>
2022-10-19 11:44:54 +0900

    Revert "signup 페이지 추가"

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Index from './pages/Index';
 import Item from './pages/Item';
 import Join from './pages/Join';
 import Login from './pages/Login';
-import Signup1 from './pages/Signup1';
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <Route path="/item" element={<Item />} />
       <Route path="/join" element={<Join />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup1 />} />
     </Routes>
   );
 }
